12ccd482ed40fae14ca88085b1bcb0c4e000e45d
Adam Wenham <adamwenham64@gmail.com>
2013-08-13 21:11:14 +0100

    updated app layout and goals validation

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -7,9 +7,14 @@
 #  description :text
 #  created_at  :datetime
 #  updated_at  :datetime
+#  measurement :string(255)
+#  steps       :text
+#  tags        :string(255)
+#  start_date  :date
+#  end_date    :date
 #
 
 class Goal < ActiveRecord::Base
   validates :title, uniqueness: true
-  validates :title, :description, presence: true
+  validates :title, :description, :measurement, :steps, :tags, presence: true
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -7,6 +7,11 @@
 #  description :text
 #  created_at  :datetime
 #  updated_at  :datetime
+#  measurement :string(255)
+#  steps       :text
+#  tags        :string(255)
+#  start_date  :date
+#  end_date    :date
 #
 
 require 'spec_helper'
